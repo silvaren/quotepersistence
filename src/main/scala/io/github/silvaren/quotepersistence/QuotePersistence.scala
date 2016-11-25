2201659f158c3130586747c9b30ea3119f48c43d
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-25 02:32:22 -0200

    Add test item insertion to mongo

--- src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
+++ src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
@@ -1,16 +1,53 @@
 package io.github.silvaren.quotepersistence
 
+import java.util.concurrent.TimeUnit
+
 import io.github.silvaren.quoteparser.Quote
-import org.mongodb.scala.{MongoClient, MongoDatabase}
+import org.mongodb.scala.{Completed, MongoClient, Observer}
+import org.mongodb.scala.bson.collection.immutable.Document
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object QuotePersistence {
 
   def persist(quotes: List[Quote]): Unit = {
     val mongoClient: MongoClient = MongoClient()
 
-    val database = mongoClient.getDatabase("quotes")
+    val database = mongoClient.getDatabase("quotedb")
 
-    mongoClient.close()
+    val collection = database.getCollection("test")
+
+    val doc = Document("name" -> "MongoDB", "type" -> "database",
+      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
+
+    val p = Promise[String]()
+    val f = p.future
+
+    val producer = Future {
+      collection.insertOne(doc).subscribe(new Observer[Completed] {
+        override def onNext(result: Completed): Unit = {
+          println("Inserted");
+          mongoClient.close()
+        }
+
+        override def onError(e: Throwable): Unit = {
+          println("Failed", e);
+          mongoClient.close()
+          p.failure(e)
+        }
+
+        override def onComplete(): Unit = {
+          println("Completed");
+          mongoClient.close()
+          p.success("Success!")
+        }
+      })
+    }
+
+    Await.result(f, Duration(10, TimeUnit.SECONDS))
+    f.foreach(x => println(x))
   }
 
   def main(args: Array[String]): Unit = {
