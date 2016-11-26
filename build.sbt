d0603715fe2b7fb060d2ad3d9fff69993bf23f46
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-26 01:44:59 -0200

    Add correct quote to json serialization

--- build.sbt
+++ build.sbt
@@ -4,10 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.11.0"
 
+ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
+
 crossPaths := false
 
 libraryDependencies += "io.github.silvaren" % "quoteparser" % "1.0"
 libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "1.1.1"
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.0"
+libraryDependencies += "com.fatboyindustrial.gson-jodatime-serialisers" % "gson-jodatime-serialisers" % "1.4.0"
 
 
 
--- src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
+++ src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
@@ -2,7 +2,11 @@ package io.github.silvaren.quotepersistence
 
 import java.util.concurrent.TimeUnit
 
-import io.github.silvaren.quoteparser.Quote
+import com.fatboyindustrial.gsonjodatime.Converters
+import com.google.gson.stream.{JsonReader, JsonWriter}
+import com.google.gson.{Gson, GsonBuilder, JsonElement, TypeAdapter}
+import io.github.silvaren.quoteparser.{Quote, StockQuote}
+import org.joda.time.{DateTime, DateTimeZone}
 import org.mongodb.scala.{Completed, MongoClient, Observer}
 import org.mongodb.scala.bson.collection.immutable.Document
 
@@ -12,40 +16,73 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object QuotePersistence {
 
+  def buildDate(year: Int, month: Int, day: Int): DateTime = {
+    val d = new DateTime()
+    d.withZone(DateTimeZone.forID("America/Sao_Paulo"))
+      .withYear(year)
+      .withMonthOfYear(month)
+      .withDayOfMonth(day)
+      .withHourOfDay(18)
+      .withMinuteOfHour(0)
+      .withSecondOfMinute(0)
+      .withMillisOfSecond(0)
+  }
+
   def persist(quotes: List[Quote]): Unit = {
-    val mongoClient: MongoClient = MongoClient()
+    val ExpectedStockQuote = StockQuote("PETR4", buildDate(2015, 1, 2), new java.math.BigDecimal("9.99"), BigDecimal("9.99"),
+      BigDecimal("9.36"), BigDecimal("9.36"), 48837200, 39738)
 
-    val database = mongoClient.getDatabase("quotedb")
+    val gsonBuilder = Converters.registerDateTime(new GsonBuilder())
+    val bigDecimalAdapter = new TypeAdapter[BigDecimal] {
+      override def write(out: JsonWriter, value: BigDecimal): Unit = {
+        out.jsonValue(value.toString)
+      }
 
-    val collection = database.getCollection("test")
-
-    val doc = Document("name" -> "MongoDB", "type" -> "database",
-      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
-
-    val p = Promise[String]()
-    val f = p.future
-
-    val producer = Future {
-      collection.insertOne(doc).subscribe(new Observer[Completed] {
-        override def onNext(result: Completed): Unit = {
-          println("Inserted");
-        }
-
-        override def onError(e: Throwable): Unit = {
-          println("Failed", e);
-          p.failure(e)
-        }
-
-        override def onComplete(): Unit = {
-          println("Completed");
-          p.success("Success!")
-        }
-      })
+      override def read(in: JsonReader): BigDecimal = BigDecimal(in.nextString())
     }
+    val gson = gsonBuilder.registerTypeAdapter(classOf[BigDecimal], bigDecimalAdapter).create()
+    val jsonQuote = gson.toJson(ExpectedStockQuote)
+    println(jsonQuote)
+    println(gson.toJson(BigDecimal("12.23")))
 
-    f.foreach(x => println(x))
-    Await.result(f, Duration(10, TimeUnit.SECONDS))
-    mongoClient.close()
+    println(gson.fromJson(jsonQuote, classOf[StockQuote]))
+
+
+//
+//    val mongoClient: MongoClient = MongoClient()
+//
+//    val database = mongoClient.getDatabase("quotedb")
+//
+//    val collection = database.getCollection("test")
+//
+//
+//    val doc = Document("name" -> "MongoDB", "type" -> "database",
+//      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
+//
+//    val p = Promise[String]()
+//    val f = p..future
+//
+//    val producer = Future {
+//      collection.insertOne(doc).subscribe(new Observer[Completed] {
+//        override def onNext(result: Completed): Unit = {
+//          println("Inserted");
+//        }
+//
+//        override def onError(e: Throwable): Unit = {
+//          println("Failed", e);
+//          p.failure(e)
+//        }
+//
+//        override def onComplete(): Unit = {
+//          println("Completed");
+//          p.success("Success!")
+//        }
+//      })
+//    }
+//
+//    f.foreach(x => println(x))
+//    Await.result(f, Duration(10, TimeUnit.SECONDS))
+//    mongoClient.close()
   }
 
   def main(args: Array[String]): Unit = {
