63c90fc7a09704c3927afa0c7b6094c637621cd7
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-26 01:47:34 -0200

    Add storing of quote documents in mongo

--- src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
+++ src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
@@ -46,43 +46,37 @@ object QuotePersistence {
     println(gson.toJson(BigDecimal("12.23")))
 
     println(gson.fromJson(jsonQuote, classOf[StockQuote]))
+    
+    val mongoClient: MongoClient = MongoClient()
 
+    val database = mongoClient.getDatabase("quotedb")
 
-//
-//    val mongoClient: MongoClient = MongoClient()
-//
-//    val database = mongoClient.getDatabase("quotedb")
-//
-//    val collection = database.getCollection("test")
-//
-//
-//    val doc = Document("name" -> "MongoDB", "type" -> "database",
-//      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
-//
-//    val p = Promise[String]()
-//    val f = p..future
-//
-//    val producer = Future {
-//      collection.insertOne(doc).subscribe(new Observer[Completed] {
-//        override def onNext(result: Completed): Unit = {
-//          println("Inserted");
-//        }
-//
-//        override def onError(e: Throwable): Unit = {
-//          println("Failed", e);
-//          p.failure(e)
-//        }
-//
-//        override def onComplete(): Unit = {
-//          println("Completed");
-//          p.success("Success!")
-//        }
-//      })
-//    }
-//
-//    f.foreach(x => println(x))
-//    Await.result(f, Duration(10, TimeUnit.SECONDS))
-//    mongoClient.close()
+    val collection = database.getCollection("test")
+
+    val p = Promise[String]()
+    val f = p.future
+
+    val producer = Future {
+      collection.insertOne(Document(jsonQuote)).subscribe(new Observer[Completed] {
+        override def onNext(result: Completed): Unit = {
+          println("Inserted");
+        }
+
+        override def onError(e: Throwable): Unit = {
+          println("Failed", e);
+          p.failure(e)
+        }
+
+        override def onComplete(): Unit = {
+          println("Completed");
+          p.success("Success!")
+        }
+      })
+    }
+
+    f.foreach(x => println(x))
+    Await.result(f, Duration(10, TimeUnit.SECONDS))
+    mongoClient.close()
   }
 
   def main(args: Array[String]): Unit = {
