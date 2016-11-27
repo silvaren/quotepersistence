4d4eecffa52bbccb40a5d110207cb9e79dd523e4
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-27 21:29:48 -0200

    Add unit test for quote serialization to mongo documents

--- build.sbt
+++ build.sbt
@@ -13,6 +13,9 @@ libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "1.1.1"
 libraryDependencies += "com.google.code.gson" % "gson" % "2.8.0"
 libraryDependencies += "com.fatboyindustrial.gson-jodatime-serialisers" % "gson-jodatime-serialisers" % "1.4.0"
 
+libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test->default"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
 
 
 
--- src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
+++ src/main/scala/io/github/silvaren/quotepersistence/QuotePersistence.scala
@@ -1,86 +1,46 @@
 package io.github.silvaren.quotepersistence
 
-import java.util.concurrent.TimeUnit
-
 import com.fatboyindustrial.gsonjodatime.Converters
 import com.google.gson.stream.{JsonReader, JsonWriter}
-import com.google.gson.{Gson, GsonBuilder, JsonElement, TypeAdapter}
-import io.github.silvaren.quoteparser.{Quote, StockQuote}
-import org.joda.time.{DateTime, DateTimeZone}
-import org.mongodb.scala.{Completed, MongoClient, Observer}
+import com.google.gson.{Gson, GsonBuilder, TypeAdapter}
+import io.github.silvaren.quoteparser.Quote
 import org.mongodb.scala.bson.collection.immutable.Document
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future, Promise}
-import scala.concurrent.ExecutionContext.Implicits.global
+import org.mongodb.scala.{Completed, MongoClient, MongoCollection, Observer}
 
 object QuotePersistence {
 
-  def buildDate(year: Int, month: Int, day: Int): DateTime = {
-    val d = new DateTime()
-    d.withZone(DateTimeZone.forID("America/Sao_Paulo"))
-      .withYear(year)
-      .withMonthOfYear(month)
-      .withDayOfMonth(day)
-      .withHourOfDay(18)
-      .withMinuteOfHour(0)
-      .withSecondOfMinute(0)
-      .withMillisOfSecond(0)
-  }
+  type MongoDocument = org.mongodb.scala.Document
 
-  def persist(quotes: List[Quote]): Unit = {
-    val ExpectedStockQuote = StockQuote("PETR4", buildDate(2015, 1, 2), new java.math.BigDecimal("9.99"), BigDecimal("9.99"),
-      BigDecimal("9.36"), BigDecimal("9.36"), 48837200, 39738)
-
+  private[this] def createGson(): Gson = {
     val gsonBuilder = Converters.registerDateTime(new GsonBuilder())
     val bigDecimalAdapter = new TypeAdapter[BigDecimal] {
       override def write(out: JsonWriter, value: BigDecimal): Unit = {
         out.jsonValue(value.toString)
       }
-
       override def read(in: JsonReader): BigDecimal = BigDecimal(in.nextString())
     }
-    val gson = gsonBuilder.registerTypeAdapter(classOf[BigDecimal], bigDecimalAdapter).create()
-    val jsonQuote = gson.toJson(ExpectedStockQuote)
-    println(jsonQuote)
-    println(gson.toJson(BigDecimal("12.23")))
+    gsonBuilder.registerTypeAdapter(classOf[BigDecimal], bigDecimalAdapter).create()
+  }
 
-    println(gson.fromJson(jsonQuote, classOf[StockQuote]))
-    
+
+  def persist(quotes: List[Quote], dbName: String, collection: String, insertCallback: Observer[Completed]): Unit = {
     val mongoClient: MongoClient = MongoClient()
+    val database = mongoClient.getDatabase(dbName)
+    val dbCollection = database.getCollection(collection)
 
-    val database = mongoClient.getDatabase("quotedb")
+    persistToDatabaseCollection(quotes, dbCollection, insertCallback)
 
-    val collection = database.getCollection("test")
-
-    val p = Promise[String]()
-    val f = p.future
-
-    val producer = Future {
-      collection.insertOne(Document(jsonQuote)).subscribe(new Observer[Completed] {
-        override def onNext(result: Completed): Unit = {
-          println("Inserted");
-        }
-
-        override def onError(e: Throwable): Unit = {
-          println("Failed", e);
-          p.failure(e)
-        }
-
-        override def onComplete(): Unit = {
-          println("Completed");
-          p.success("Success!")
-        }
-      })
-    }
-
-    f.foreach(x => println(x))
-    Await.result(f, Duration(10, TimeUnit.SECONDS))
     mongoClient.close()
   }
 
-  def main(args: Array[String]): Unit = {
-    persist(List())
+  def persistToDatabaseCollection(quotes: List[Quote], dbCollection: MongoCollection[MongoDocument],
+                                  insertCallback: Observer[Completed]): Unit = {
+    val quoteDocs: List[MongoDocument] = serializeQuotesAsMongoDocuments(quotes)
+    dbCollection.insertMany(quoteDocs).subscribe(insertCallback)
   }
 
+  def serializeQuotesAsMongoDocuments(quotes: List[Quote]): List[MongoDocument] = {
+    val gson = createGson()
+    quotes.map(q => Document(gson.toJson(q)))
+  }
 }
