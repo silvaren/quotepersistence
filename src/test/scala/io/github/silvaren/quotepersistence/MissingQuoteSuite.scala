b60d384dade8a73ec751c144fa40f0e7de021612
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-12-29 02:05:20 -0200

    Add unit test for holidays exclusion

--- src/test/scala/io/github/silvaren/quotepersistence/MissingQuoteSuite.scala
+++ src/test/scala/io/github/silvaren/quotepersistence/MissingQuoteSuite.scala
@@ -11,8 +11,16 @@ class MissingQuoteSuite extends FunSuite {
   test("only includes weekdays") {
     val missingDates = MissingQuote.gatherMissingDates(Util.buildDate(2015, 12, 30), Set(), Util.buildDate(2016, 8, 18))
 
-    assert(!missingDates.exists(d => d.dayOfWeek().get() === DateTimeConstants.SATURDAY ||
-      d.dayOfWeek().get() === DateTimeConstants.SUNDAY))
+    assert(missingDates.forall(d => d.dayOfWeek().get() != DateTimeConstants.SATURDAY &&
+      d.dayOfWeek().get() != DateTimeConstants.SUNDAY))
+  }
+
+  test("does not include holidays") {
+    val holidays = Set(Util.buildDate(2016, 1, 1), Util.buildDate(2016, 2, 8), Util.buildDate(2016, 2, 9))
+    val missingDates = MissingQuote.gatherMissingDates(Util.buildDate(2015, 12, 30), holidays,
+      Util.buildDate(2016, 8, 18))
+
+    assert(missingDates.forall(d => !holidays.contains(d)))
   }
 
 }
